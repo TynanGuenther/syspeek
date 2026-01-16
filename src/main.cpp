3f601daee9920e69c78d5a6945cb0df47b0f10a7
TynanGuenther <tgunther701@gmail.com>
2026-01-16 15:34:39 -0600

    Add hostname and kernel information

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/utsname.h>
 
 int main() {
-    std::cout << "syspeek running\n";
+
+    //Hostname
+    char hostname[256];
+    if (gethostname(hostname, sizeof(hostname)) != 0) {
+	perror("gethostname");
+	return 1;
+    }
+
+    //Kernel info
+    struct utsname sysinfo;
+    if (uname(&sysinfo) != 0) {
+	perror("uname");
+	return 1;
+    }
+
+    std::cout << "Hostname: " << hostname << "\n";
+    std::cout << "Kernel: " << sysinfo.sysname << " " << sysinfo.release << "\n";
+
     return 0;
+
 }
 
