ce5fc5443cce790a72353e86a8ea50b6b4173407
TynanGuenther <tgunther701@gmail.com>
2026-01-16 15:51:01 -0600

    Add system uptime

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <unistd.h>
 #include <sys/utsname.h>
+#include <fstream>
+
+void print_uptime() {
+    std::ifstream file ("/proc/uptime");
+    if (!file) {
+	std::cerr << "Failed to open /proc/uptime\n";
+	return;
+    }
+
+    double seconds;
+    file >> seconds;
+
+    long total_minutes = static_cast<long>(seconds) / 60;
+    long minutes = total_minutes % 60;
+    long total_hours = total_minutes / 60;
+    long hours = total_hours % 24;
+    long days = total_hours / 24;
+
+    std::cout << "Uptime: "
+	      << days << " days, "
+	      << hours << " hours, "
+	      << minutes << " minutes\n";
+}
 
 int main() {
 
@@ -20,6 +43,7 @@ int main() {
 
     std::cout << "Hostname: " << hostname << "\n";
     std::cout << "Kernel: " << sysinfo.sysname << " " << sysinfo.release << "\n";
+    print_uptime();
 
     return 0;
 
